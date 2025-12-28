df4dc81e2141d62ab5d8b87817097dd919ee14f6
Raj Kumar <rajkr2051@gmail.com>
2025-12-28 14:44:49 +0530

    Add backend and app, setup gitignore

---
+++ backend/treatments.py
@@ -0,0 +1,12 @@
+TREATMENTS = {
+    "Tomato___Late_blight": {
+        "cause": "Fungal infection",
+        "treatment": "Use copper-based fungicide",
+        "prevention": "Avoid overhead watering"
+    },
+    "Potato___Early_blight": {
+        "cause": "Fungal disease",
+        "treatment": "Apply chlorothalonil spray",
+        "prevention": "Crop rotation"
+    }
+}
