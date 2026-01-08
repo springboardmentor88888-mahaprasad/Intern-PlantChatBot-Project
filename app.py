609711a373c21c82d6f85a6d390bc7281456c8c6
Neeraj S <neerajleo19@gmail.com>
2026-01-08 19:47:11 +0530

    Updated backend

--- app.py
+++ app.py
@@ -34,8 +34,15 @@ model, class_names = load_model()
 transform = transforms.Compose([
     transforms.Resize((IMG_SIZE, IMG_SIZE)),
     transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
 ])
 
+if uploaded_file is None:
+    st.warning("Please upload a leaf image")
+    st.stop()
 # ---------------- UI ----------------
 st.title("🌿 Plant Disease Predictor")
 st.write("Upload a leaf image to detect plant disease")
@@ -44,14 +51,22 @@ uploaded_file = st.file_uploader("Choose an image", type=["jpg", "png", "jpeg"])
 
 if uploaded_file:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, width="stretch")
+    st.image(image, width=400)
+
 
     img_tensor = transform(image).unsqueeze(0)
 
     with torch.no_grad():
         outputs = model(img_tensor)
-        probs = F.softmax(outputs, dim=1)
-        confidence, pred_idx = torch.max(probs, 1)
+        probs = F.softmax(outputs, dim=1)[0]
+        
+        top3 = torch.topk(probs,3)
+        
+        st.subheader("Top Predictions")
+        for i in range(3):
+            label = class_names[top3.indices[i].item()]
+            conf = top3.values[i].item() * 100
+            st.write(f"{i+1}.{label} - {conf:.2f}%")
 
     st.success(f"Prediction: **{class_names[pred_idx.item()]}**")
     st.info(f"Confidence: **{confidence.item() * 100:.2f}%**")
@@ -75,3 +90,9 @@ def text_based_diagnosis(user_text):
 if user_text:
     text_prediction = text_based_diagnosis(user_text)
     st.warning(f"Text-based diagnosis:**{text_prediction}**")
+if user_text and uploaded_file:
+    st.markdown("###Final diagnosis summary")
+    st.success(
+        f"Image-based: {best_label}\n\n"
+
+    )
