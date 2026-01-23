55152b35ba5ab6693f1ff71c0eeb5027266e07ad
Abhi2203-Engg <abhilasharathi22@gmail.com>
2026-01-23 19:02:43 +0530

    My work updated
    Updated treatments.py code file

--- backend/treatments.py
+++ backend/treatments.py
@@ -2,11 +2,11 @@
 
 TREATMENTS= {
    "Tomato__Late_blight": "Use copper-based fungicide, Avold overhead satering.",
-   "Tomato__Early_blight": "Remove infected leaves. Apply mancozeb or chlorotiuion."
-   "Potato__Late_blight" : "Spray fungicide and remove infected plants inmediately." 
+   "Tomato__Early_blight": "Remove infected leaves. Apply mancozeb or chlorotiuion.",
+   "Potato__Late_blight" : "Spray fungicide and remove infected plants inmediately.",
    "Potato__Early_blight": "Apply appropriate fungicide and practice crop rotation.async",
    "Pepper__Bacterial_spot": "Use disease-free seeds and copper sprays.",
-   "Healthy"! "No treatment needed. Maintain proper irrigation and nutrition."
+   "Healthy": "No treatment needed. Maintain proper irrigation and nutrition."
 }
 
 def get_treatment(disease_name):
